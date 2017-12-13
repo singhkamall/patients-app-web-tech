761530894dc4ab12e632ee4dad881a6d12c7f58f
Robert Argume <robert@Roberts-MacBook-Pro.local>
2017-12-13 06:16:50 -0500

    Update clinical-data html and js to retrieve data from api

--- app/Views/clinical-data/clinical-data.js
+++ app/Views/clinical-data/clinical-data.js
@@ -1,7 +1,4 @@
 'use strict';
-$(document).ready(function(){
-    $("[data-toggle=tooltip]").tooltip();
-});
 
 angular.module('myApp.clinical-data', ['ngRoute'])
 
@@ -12,6 +9,60 @@ angular.module('myApp.clinical-data', ['ngRoute'])
         });
     }])
 
-    .controller('ClinicalDataController', [function () {
+    .controller('ClinicalDataController', function ($scope, $http) {
+        $http.get("http://127.0.0.1:5000/patients/1/records")
+            .then(function (response) {
+                $scope.records = response.data;
+            });
 
-    }]);
+    });
+
+
+// angular.module('myApp.clinical-data', ['ngRoute', 'ngResource'])
+
+//     .config(['$routeProvider', function ($routeProvider) {
+//         $routeProvider.when('/clinical-data', {
+//             templateUrl: 'Views/clinical-data/clinical-data.html',
+//             controller: 'ClinicalDataController'
+//         });
+//     }])
+
+//     .factory('RecordsService', function ($resource) {
+//         var data = $resource('http://127.0.0.1:5000/patients/:patientId/records/:recordId',
+//                                     {patientId:'@patientId', recordId:'@recordId'},
+//                                     {update:{ method:'PUT'}} );
+
+
+//         return data;
+//     })
+
+//     .controller('ClinicalDataController', ['$scope', '$resource', function ($scope, $resource) {
+
+//         // Using Factory to create a service
+//          $scope.records = RecordsService.query( {patientId:1});
+
+        // //GET Action Method for ALL records of a specific patient
+        // var AllRecords = $resource('http://127.0.0.1:5000/patients/:patientId/records', {patientId:'@patientId'});
+        // AllRecords.query( {patientId:1}, function(records){
+        //     $scope.records = records;
+        // })
+
+        // //GET Action Method for ONE specific record of a specific patient
+        // var OneRecord = $resource('/patients/:patientId/records/:recordId', {patientId:'@patientId', recordId:'@recordId'});
+        // OneRecord.get( {patientId:1, recordId:1}, function(record){
+        //     $scope.record = record;
+        // })
+        
+        // //GET Action Method for all records 
+        // var CriticalPatients = $resource('/patients/criticalCondition');
+        // CriticalPatients.query(function(criticalPatients){
+        //     $scope.criticalPatients = criticalPatients;					
+        // });
+    //}]);
+
+
+    // { 'get':    {method:'GET'},
+    // 'save':   {method:'POST'},
+    // 'query':  {method:'GET', isArray:true},
+    // 'remove': {method:'DELETE'},
+    // 'delete': {method:'DELETE'} };
