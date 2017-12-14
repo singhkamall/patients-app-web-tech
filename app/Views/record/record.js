864adeb307ed0160f9bafb71dc5707f2dc9b8c68
Robert Argume <robert@Roberts-MacBook-Pro.local>
2017-12-14 17:18:16 -0500

    Add create clinical data record functionality

--- app/Views/record/record.js
+++ app/Views/record/record.js
@@ -11,24 +11,29 @@ angular.module('myApp.record', ['ngRoute'])
             templateUrl: 'Views/record/record.html',
             controller: 'RecordController'
         });
-        
+        $routeProvider.when('/patients/:patientId/createRecords', {
+            templateUrl: 'Views/record/create-record.html',
+            controller: 'RecordController'
+        });
     }])
 
     .controller('RecordController', function ($scope, $http, $routeParams, $location) {
         $scope.patientId = $routeParams.patientId;
         $scope.recordId = $routeParams.recordId;
-
-        // By default, we assume "View action". Disable text input and hide Save Button
+        
+        // Patient information is always readonly
         $scope.patientDataReadOnly = true;
-        $scope.recordDataReadOnly = true;
-        $scope.showButton = false;
+
+        // If "Edit" or "Create" Actions, enable text inputs and show Save Button
+        $scope.recordDataReadOnly = false;
+        $scope.showButton = true;
 
         // Get action from local url
         var segments = $location.path().split("/");
-        if(segments[3] && segments[3] == "editRecords") {
-            // If "Edit Action", enable text inputs and show Save Button
-            $scope.recordDataReadOnly = false;
-            $scope.showButton = true;
+        if(segments[3] && segments[3] == "records") {
+            // "View action". Disable text input and hide Save Button
+            $scope.recordDataReadOnly = true;
+            $scope.showButton = false;
         }
 
         $http.get("http://127.0.0.1:5000/patients/" + $scope.patientId)
@@ -36,14 +41,33 @@ angular.module('myApp.record', ['ngRoute'])
             $scope.patient = response.data;
         });
 
-        $http.get("http://127.0.0.1:5000/patients/" + $scope.patientId + "/records/" + $scope.recordId)
-        .then(function (response) {
-            $scope.recordDetails = response.data;
-        });
+        if( $scope.recordId ) {
+            $http.get("http://127.0.0.1:5000/patients/" + $scope.patientId + "/records/" + $scope.recordId)
+            .then(function (response) {
+                $scope.recordDetails = response.data;
+            });
+        }   
 
         $scope.updateRecord = function(){
-            $http.put("http://127.0.0.1:5000/patients/" + $scope.patientId + "/records/" + $scope.recordId, 
+            if( $scope.recordId ) {
+                $http.put("http://127.0.0.1:5000/patients/" + $scope.patientId + "/records/" + $scope.recordId, 
+                {
+                    "Practitioner": $scope.recordDetails.Practitioner,
+                    "MedicalCenter": $scope.recordDetails.MedicalCenter,
+                    "DateTime": $scope.recordDetails.DateTime,
+                    "DataType": $scope.recordDetails.DataType,
+                    "Reading": $scope.recordDetails.Reading
+                })
+                .then(function(response){
+                    console.log(response);
+                })
+            }
+        };
+
+        $scope.createRecord = function(){
+            $http.post("http://127.0.0.1:5000/patients/" + $scope.patientId + "/records", 
             {
+                "PatientID": $scope.recordDetails.PatientID,
                 "Practitioner": $scope.recordDetails.Practitioner,
                 "MedicalCenter": $scope.recordDetails.MedicalCenter,
                 "DateTime": $scope.recordDetails.DateTime,
