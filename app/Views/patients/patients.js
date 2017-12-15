83e1d799c812e2ed3c5876efed915c8cb4861b12
Robert Argume <robert@Roberts-MacBook-Pro.local>
2017-12-14 23:33:15 -0500

    Add create patient page

--- app/Views/patients/patients.js
+++ app/Views/patients/patients.js
@@ -7,6 +7,10 @@ angular.module('myApp.patients', ['ngRoute'])
             templateUrl: 'Views/patients/patients.html',
             controller: 'PatientsController'
         });
+        $routeProvider.when('/createPatients', {
+            templateUrl: 'Views/patients/create-patient.html',
+            controller: 'PatientsController'
+        });
         $routeProvider.when('/patients/criticalPatients', {
             templateUrl: 'Views/patients/patients.html',
             controller: 'PatientsController'
@@ -17,6 +21,10 @@ angular.module('myApp.patients', ['ngRoute'])
         // By default we config api call and page title for showing all patients
         $scope.title = "Patients";
         let apiUrl = "http://127.0.0.1:5000/patients";
+
+        // If "Edit" or "Create" Actions, enable text inputs and show Save Button
+        $scope.recordDataReadOnly = false;
+        $scope.showButton = true;
 
         if( $location.path().split("/").length == 3 ) {
             // If path has one more component, then we config api call and title for critical patients
@@ -29,5 +37,27 @@ angular.module('myApp.patients', ['ngRoute'])
                 console.log(response);
                 $scope.patients = response.data;
             });
+        
+        $scope.createPatient = function(){
+            $http.post(apiUrl, 
+            {
+                "FirstName": $scope.patients.FirstName,
+                "Address": $scope.patients.Address,
+                "DateOfBirth": $scope.patients.DateOfBirth,
+                "Gender": $scope.patients.Gender,
+                "Telephone": $scope.patients.Telephone,
+                "EmergencyContact": {
+                    "Name": $scope.patients.EmergencyContact.Name,
+                    "Address": $scope.patients.EmergencyContact.Address,
+                    "Relationship": $scope.patients.EmergencyContact.Relationship,
+                    "Telephone": $scope.patients.EmergencyContact.Telephone
+                },
+                "BloodType": $scope.patients.BloodType,
+                "IsInCritcalCondition": $scope.patients.IsInCritcalCondition
+            })
+            .then(function(response){
+                console.log(response);
+            })
+        };
 
     });
